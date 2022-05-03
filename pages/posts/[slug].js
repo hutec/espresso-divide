11cd7c36dd5e53605ed123acfb7c80657cd969fc
Robin Hutmacher <robin.hutmacher@gmail.com>
2022-05-03 16:20:49 +0200

    Remove console.log

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -16,7 +16,6 @@ export default function Post({ post, morePosts, preview }) {
   if (!router.isFallback && !post?.slug) {
     return <ErrorPage statusCode={404} />;
   }
-  console.log(post.track);
   return (
     <Layout preview={preview}>
       <Container>
